7e73909d78290bba950caf77652ce953afae31be
lincolnxlw <lincolnxlw@gmail.com>
2024-02-14 02:57:54 -0600

    Add steps to build depthai-core from source to test bug fix version

---
+++ utils/build_local.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ ! -d "tmp/depthai-ros" ]; then
+    echo "Cloning depthai-ros"
+    git clone -b noetic https://github.com/luxonis/depthai-ros.git tmp/depthai-ros/
+fi
+
+docker buildx build --load \
+    -f ./docker/Dockerfile \
+    --memory=8g \
+    --platform linux/amd64 \
+    --build-arg USE_RVIZ=1 \
+    --build-arg DEPTHAI_ROS_PATH="./tmp/depthai-ros" \
+    -t lincolnxlw/my-depthai-ros \
+    .
